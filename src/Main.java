07298c0c6df062e9322f297ad6f212afbc9d34e8
Your name <maksimdubinin@MacBook-Pro-Maksim-2.local>
2023-11-02 20:49:35 +0300

    Project created

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Order order = new Order(7);
+        System.out.println(order);
+    }
+}
